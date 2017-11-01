1998ea7b9b175564d101310da45c9d5d74c5c73a
Leigh Ciechanowski <leighciechanowski@Leighs-MacBook-Pro.local>
2017-11-01 18:13:47 +0000

    Added docker file

---
+++ Todo.Api/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Todo.Model
+{
+    public class TodoItem
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public bool IsComplete
+        {
+            get;
+            set;
+        }
+    }
+}
